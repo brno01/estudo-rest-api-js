1956863f710de9b73050dbb0084d4c944a40c709
brno01 <brunohashtagfox@gmail.com>
2022-07-07 23:37:06 -0300

    brasil

---
+++ database/mysql.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql');
+  
+const pool = mysql.createPool({
+    "connectionLimit": 2000,
+    "user": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQL_HOST,
+    "port": process.env.MYSQL_PORT,
+    "URL_API": process.env.URL_API
+});
+
+exports.execute = (query, params=[]) => {
+    return new Promise((resolve, reject) => {
+        pool.query(query, params, (error, result) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(result);
+            }
+        });
+    })
+}
+
+exports.pool = pool;
